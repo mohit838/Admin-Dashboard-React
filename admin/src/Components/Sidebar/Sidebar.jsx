435a189e57ac6c0e668a15152ef683e25280ec9b
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-14 15:15:41 +0600

    Sidebar Icon Setup

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import Inventory2Icon from "@mui/icons-material/Inventory2";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+import TocIcon from "@mui/icons-material/Toc";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
 import "./sidebar.scss";
 
 const Sidebar = () => {
@@ -11,19 +23,52 @@ const Sidebar = () => {
       <div className="center">
         <ul>
           <li>
+            <DashboardIcon />
             <span>Dashboard</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineIcon />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <Inventory2Icon />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <ShoppingCartIcon />
+            <span>Orders</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <LocalShippingIcon />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <BarChartIcon />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsActiveIcon />
+            <span>Notifications</span>
+          </li>
+          <li>
+            <MonitorHeartIcon />
+            <span>System Health</span>
+          </li>
+          <li>
+            <TocIcon />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon />
+            <span>Settings</span>
+          </li>
+          <li>
+            <AccountCircleIcon />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon />
+            <span>Log Out</span>
           </li>
         </ul>
       </div>
