5130629fd69b0fb72d48d16415375963cbd9af58
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-14 16:43:50 +0600

    Navbar Icon Setups

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,50 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="search..." />
+          <SearchIcon className="icon" />
+        </div>
+
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon" />
+            English
+          </div>
+
+          <div className="item">
+            <DarkModeIcon className="icon" />
+          </div>
+
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
