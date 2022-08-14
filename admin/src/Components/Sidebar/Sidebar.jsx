addfd641eabd490a59187b6564d17e25dfcea00f
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-14 15:01:33 +0600

    Home Page and Sidebar Logo

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import "./sidebar.scss";
 
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">GhostRi0</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">Color options</div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- admin/src/Pages/Home/Home.jsx
+++ admin/src/Pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">Container</div>
+    </div>
+  );
 };
 
 export default Home;
