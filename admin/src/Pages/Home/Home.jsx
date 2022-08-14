c7fe7a2cf8e0237a80d63cfe8bc6621e791ceec4
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-14 17:36:18 +0600

    Make Widget Complete

--- admin/src/Pages/Home/Home.jsx
+++ admin/src/Pages/Home/Home.jsx
@@ -10,11 +10,11 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        <div className="widget">
-          <Widget />
-          <Widget />
-          <Widget />
-          <Widget />
+        <div className="widgetsHome">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
       </div>
     </div>
